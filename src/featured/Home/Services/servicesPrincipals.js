a2474880165dd0ce151bb22bc6e2730d658e6c3f
LuisYepes601 <tucorreo@example.com>
2026-02-17 12:20:27 -0500

    ll

--- src/featured/Home/Services/servicesPrincipals.js
+++ src/featured/Home/Services/servicesPrincipals.js
@@ -1,6 +1,6 @@
 export const dataServices = [{
     title: "Impresiones a laser, blanco y negro y a color.",
-    image: ""
+    image: "assets/images/impresionColor.png"
 }, {
     title: "Fotocopias a laser, blanco y negro y a color.",
     image: ""
