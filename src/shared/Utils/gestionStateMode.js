37f97e55726286147210da618984491d381e01c5
LuisYepes601 <tucorreo@example.com>
2026-03-02 12:39:20 -0500

    sss

--- src/shared/Utils/gestionStateMode.js
+++ src/shared/Utils/gestionStateMode.js
@@ -1,6 +1,10 @@
 
 export function gestionStateMode() {
 
+     localStorage.setItem("stateMode", JSON.stringify({
+        isDark: "false"
+    }))
+
     const stateMode = JSON.parse(localStorage.getItem("stateMode"));
 
 
